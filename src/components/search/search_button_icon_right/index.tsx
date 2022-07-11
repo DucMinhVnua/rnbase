e0ab1dee6fc0e5109e362de66cfee8f04b8fc8b8
DucMinhVnua <64829929+DucMinhVnua@users.noreply.github.com>
2022-07-11 09:23:54 +0700

    add search_button_icon_rigt

--- src/components/search/search_button_icon_right/index.tsx
+++ src/components/search/search_button_icon_right/index.tsx
@@ -1,4 +1,4 @@
-import {TextInput, View} from 'react-native';
+import {Image, TextInput, View} from 'react-native';
 import React from 'react';
 import {
   widthPercentageToDP as wp,
@@ -11,20 +11,41 @@ interface Props {
   value?: any;
   borderWidth?: number;
   placeholderTextColor?: string;
-  fontSize: number;
-  paddingHorizontal: number;
-  paddingVertical: number;
+  fontSize?: number;
+  paddingHorizontal?: number;
+  paddingVertical?: number;
+  borderRadius?: number;
+  placeholder?: string;
+  backgroundColor?: string;
+  underlineColorAndroid?: string;
+  widthIconImage?: number;
+  heightIconImage?: number;
+  colorText?: string;
+  widthIcon?: number;
+  radiusButton?: number;
+  backgroundButtonIcon?: string;
+  imagePath: NodeRequire;
 }
 
 const SearchButtonRight = ({
   widthSearch = 50,
   onChange,
   value,
-  borderWidth = 1,
-  placeholderTextColor,
+  borderWidth = 0,
+  placeholderTextColor = '#ddd',
   fontSize = 18,
   paddingHorizontal,
   paddingVertical,
+  borderRadius = 8,
+  placeholder = 'Search...',
+  backgroundColor = '#1F232F',
+  underlineColorAndroid,
+  widthIconImage = 4,
+  heightIconImage = 2,
+  colorText = '#fff',
+  widthIcon = 50,
+  backgroundButtonIcon = '#565E70',
+  imagePath,
 }: Props) => {
   return (
     <View
@@ -33,20 +54,44 @@ const SearchButtonRight = ({
         paddingVertical: paddingVertical,
         borderWidth: borderWidth,
         position: 'absolute',
-        borderRadius: 10,
-
-        // backgroundColor: 'black',
+        borderRadius: borderRadius,
+        backgroundColor: backgroundColor,
       }}>
-      <TextInput
-        placeholderTextColor={placeholderTextColor}
-        placeholder="search"
+      <View
         style={{
-          fontSize: fontSize,
-          width: wp(`${widthSearch}%`),
-        }}
-        onChange={() => onChange}
-        value={value}
-      />
+          flexDirection: 'row',
+          alignItems: 'center',
+        }}>
+        <TextInput
+          placeholderTextColor={placeholderTextColor}
+          placeholder={placeholder}
+          style={{
+            color: colorText,
+            fontSize: fontSize,
+            width: wp(`${widthSearch}%`),
+          }}
+          onChange={() => onChange}
+          value={value}
+          underlineColorAndroid={underlineColorAndroid}
+        />
+        <View
+          style={{
+            width: widthIcon,
+            backgroundColor: backgroundButtonIcon,
+            height: '100%',
+            justifyContent: 'center',
+            alignItems: 'center',
+            borderRadius: borderRadius,
+          }}>
+          <Image
+            style={{
+              width: wp(`${widthIconImage}%`),
+              height: hp(`${heightIconImage}%`),
+            }}
+            source={imagePath}
+          />
+        </View>
+      </View>
     </View>
   );
 };
